81dc59847b4766e2a6a7f469198277a65388899e
Ash-Bash <xpsx.fan@googlemail.com>
2016-05-20 19:22:50 +0100

    Added Event APIs to delegate.js

--- main.js
+++ main.js
@@ -2,6 +2,13 @@ const electron = require('electron');
 const {app} = electron;
 const {BrowserWindow} = electron;
 
+var path = require('path');
+global.appRoot = path.resolve(__dirname);
+
+const delegate = require('./src/scripts/delegate.js');
+
+let app_title = "Zen Code Editor";
+let app_version = app.getVersion();
 let window;
 
 function createWindow() {
@@ -9,14 +16,54 @@ function createWindow() {
     //Create The Browser Window
     window = new BrowserWindow({ width: 800, height: 600 });
     
-    //Load the index.html of the app.
-    window.loadURL('file://' + __dirname + '/src/layouts/index.html');
+    //Set Title of Window.
+    window.setTitle(app_title);
     
     //Emitted When The Window Is Closed.
+    window.on('responsive', () => {
+        delegate.windowIsResponsive();
+    });
+    
+    window.on('unresponsive', () => {
+        delegate.windowIsNotResponsive();
+    });
+    
+    window.on('restore', () => {
+        delegate.windowDidResume();
+    });
+    
+    window.on('hide', () => {
+        delegate.windowDidPause();
+    });
+    
+    window.on('maximize', () => {
+        delegate.windowDidMaximize();
+    });
+    
+    window.on('unmaximize', () => {
+        delegate.windowDidUnmaxinmized();
+    });
+    
     window.on('closed', () => {
+        delegate.windowDidClose();
         window = null;
     });
+    
+    delegate.applicationDidFinnishLoading(window);
 }
+
+app.setName(app_title);
+
+app.on('will-finish-launching', () => {
+    console.log('-----------------------------------');
+    console.log('App Name: ' + app_title);
+    console.log('App Version: ' + app_version);
+    console.log('Developed By: Ashley James Chapman');
+    console.log('-----------------------------------');
+    console.log(' ');
+    
+    delegate.applicationWillFinnishLoading();
+});
 
 app.on('ready', createWindow);
 
@@ -27,7 +74,17 @@ app.on('window-all-closed', () => {
     }
 });
 
+app.on('before-quit', () => {
+    delegate.applicationWillClose();
+});
+
+app.on('will-quit', () => {
+    delegate.applicationDidClose();
+});
+
 app.on('activate', () => {
+    
+    delegate.applicationDidResume();
     
     //Dock Icon Is Clicked And There Are No Other Windows Open.
     if (window == null){
---
+++ src/scripts/delegate.js
@@ -0,0 +1,82 @@
+//Delegation Script
+
+let win;
+
+function applicationWillFinnishLoading(){
+    console.log('Application Will Finnish Loading');
+}
+
+function applicationDidFinnishLoading(window){
+    console.log('Application Did Finnish Loading');
+    win = window;
+    
+    //Load the index.html of the app.
+    window.loadURL('file://' + appRoot + '/src/layouts/index.html');
+}
+    
+function applicationDidResume(){
+    console.log('Application Did Resume');
+}
+
+function applicationWillClose(){
+    console.log('Application Will Close');
+}
+    
+function applicationDidClose(){
+    console.log('Application Did Close');
+}
+
+function windowDidMaximize(){
+    console.log('Window Did Maximize');
+}
+
+function windowDidUnmaxinmized(){
+    console.log('Window Did Unmaximized');
+}
+
+function windowDidResume(){
+    console.log('Window Did Resume');
+}
+
+function windowDidPause() {
+    console.log('Window Did Pause');
+}
+
+function windowIsResponsive(){
+    console.log('Window Is Responsive');
+}
+
+function windowIsNotResponsive(){
+    console.log('Window Is Not Responsive');
+}
+
+function windowDidClose(){
+    console.log('Window Did Close');
+}
+
+module.exports = {
+    applicationWillFinnishLoading: applicationWillFinnishLoading,
+    
+    applicationDidFinnishLoading: applicationDidFinnishLoading,
+    
+    applicationDidResume: applicationDidResume,
+    
+    applicationWillClose: applicationWillClose,
+    
+    applicationDidClose: applicationDidClose,
+    
+    windowDidMaximize: windowDidMaximize,
+    
+    windowDidUnmaxinmized: windowDidUnmaxinmized,
+    
+    windowDidResume: windowDidResume,
+    
+    windowDidPause: windowDidPause,
+    
+    windowIsResponsive: windowIsResponsive,
+    
+    windowIsNotResponsive: windowIsNotResponsive,
+    
+    windowDidClose: windowDidClose
+    
+}
