bee64458587dd6214d2fc2a10a17f5893e9ecffe
Ash-Bash <xpsx.fan@googlemail.com>
2016-06-04 01:06:34 +0100

    last Miniute tweaks

--- src/scripts/controllers/VideoViewerController.js
+++ src/scripts/controllers/VideoViewerController.js
@@ -7,6 +7,25 @@
   function VideoViewerController ($scope, $log) {
     var ipc = electron.ipcRenderer;
 
-    $scope.MainStaticsStyleFile = "../styles/themes/zen-dark-theme/Statics.css"
+    var themeListJsonFile = fs.readFileSync('./src/styles/themes/theme_list.json');
+    var themeListData = JSON.parse(themeListJsonFile);
+    var themeList = themeListData.list;
+    console.log(JSON.parse(themeListJsonFile));
+    var themeJsonFile = fs.readFileSync(themeList[0].dir);
+    var themeJsonData = JSON.parse(themeJsonFile);
+    console.log(themeJsonData);
+    var themeData = themeJsonData.theme;
+
+
+    //var theme = themeManager.ThemeManager('');
+
+    var editorSchema = themeData[0].colors;
+    var infobarSchema = themeData[1].colors;
+    var textColorSchema = themeData[2].colors;
+
+    $scope.BackgroundStyle = {
+        'background-color': editorSchema.primary
+    };
+
   }
 })();
